2f35f9675a07cb823f118e349b472127a77d0e9b
jonathansayer <jon.sayer1@gmail.com>
2015-11-22 17:46:41 +0000

    removed show method from controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,11 @@
 class ProfilesController < ApplicationController
 
   def index
-
   end
 
   def search
     q = params[:profile][:name]
     @response = HTTParty.get('https://api.github.com/search/users?q=' + q )
-
-  end
-
-  def show
-    @response = session[:response]
   end
 
 end
